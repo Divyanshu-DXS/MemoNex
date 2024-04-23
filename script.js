b9a0da0fbaa5600bfbd1f1399335e26c229786b7
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2024-04-23 18:26:42 -0400

    Adding THEME TOGGLE SWITCH to the app

--- script.js
+++ script.js
@@ -40,3 +40,38 @@ document.addEventListener("keydown", event=>{
         event.preventDefault();
     }
 })
+
+// Select the toggle switch input element
+const themeToggle = document.getElementById('theme-toggle');
+
+// Add event listener for change event
+themeToggle.addEventListener('change', function() {
+  // Check if the toggle switch is checked
+  if (this.checked) {
+    // If checked, set dark theme
+    console.log("clicked");
+    setDarkTheme();
+  } else {
+    console.log("clicked now");
+    // If unchecked, set light theme
+    setLightTheme();
+  }
+});
+
+let container = document.querySelector(".container");
+// Function to set dark theme
+function setDarkTheme() {
+  container.style.background="radial-gradient(circle, #1b1e23, #131416)";
+  container.style.color="#fff";
+  createBtn.style.background="linear-gradient(135deg, #2d9ee0, #1f4b99)";
+}
+
+// Function to set light theme
+function setLightTheme() {
+    // Set light theme background gradient
+    container.style.background = "radial-gradient(circle, #e6e6e6, #ffffff)";// Set background color to light
+    container.style.color = '#333'; // Set text color to dark
+    createBtn.style.background = "linear-gradient(135deg, #555555, #000000)";
+
+}
+
